6c7882cfcc93bae73ebc6e66c4275477216de605
kayitare2000 <kayeric2018@gmail.com>
2024-03-25 20:33:38 +0200

    rename app title

--- success_page.php
+++ success_page.php
@@ -11,7 +11,7 @@ require('db_connect.php');
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>FERWAFA DEVELOPMENT</title>
+	<title>FERWAFA DEVELOPMENT APP</title>
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="apple-touch-icon" href="apple-touch-icon.png">
